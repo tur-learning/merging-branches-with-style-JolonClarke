beb01f3615d5321a3f5acce3226d71ecccfbda78
JolonClarke <jolon.clarke@temple.edu>
2025-02-20 17:55:33 +0000

    First commit on master: implementing add function

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import utils
+from utils import add
 
 def main():
-    print("Welcome to the Awesome Math Program!")
-    print("This code has been updated on master branch.")
-
-# What is this??
-if __name__ == "__main__":
-    main()
+    print("You are on the master branch")
--- utils.py
+++ utils.py
@@ -2,4 +2,7 @@ def add(a, b):
     """
     Returns the sum of a and b.
     """
+    res = a + b
     return a + b
+
+print("You are in utils.py")
