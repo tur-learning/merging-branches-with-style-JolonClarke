47838dc36aa34a01cf11af7a959efb5697e95874
JolonClarke <jolon.clarke@temple.edu>
2025-02-20 18:10:27 +0000

    Calling add function from the domain

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from utils import add
 
 def main():
-    print("You are on the master branch")
+    print("You are on the feature_a branch")
+    a = 10
+    b = 50
+    result = add(a, b)
+    print("The result of the addition is " + str(result) + ".")
+
+main()
